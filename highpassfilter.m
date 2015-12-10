968438bda0bf7a750de58a218f9ac13cfe0ca067
Alexander Reznicek <e1125076@student.tuwien.ac.at>
2015-12-10 17:03:47 +0100

    Filter hinzugefügt, Radioempfang nun komplett

---
+++ highpassfilter.m
@@ -0,0 +1,9 @@
+IIRFilterOrder = 10;
+Fpass = 15;
+Nth=16;
+Fs=2.5*10^6/Nth;
+IIRCoeff = designfilt('highpassiir', 'FilterOrder', IIRFilterOrder, ...
+          'PassbandFrequency', Fpass, ...
+          'StopbandAttenuation', 60, 'PassbandRipple', 1, ...
+          'SampleRate', Fs, 'DesignMethod', 'ellip');
+fvtool(IIRCoeff, 'Fs',Fs)
