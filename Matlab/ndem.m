8f5ea2eb131be4403b874df8ff7ec7e12f704626
Alexander Reznicek <e1125076@student.tuwien.ac.at>
2015-12-11 13:43:22 +0100

    Fix der falsch reingeladenen Dateien

--- Matlab/ndem.m
+++ Matlab/ndem.m
@@ -17,25 +17,25 @@ clear IQ
 
 filteredamdemodsignal=amdemodsignal;
 %Filter für die Frequenzen über 100kHz, auskommentieren schaltet Filter aus
-% open b14.mat; %b.mat=400 Punkte, b200.mat=200, b14.mat=14
-% b=ans.h';
-% xhist=zeros(length(b),1);
-% for i=1:length(amdemodsignal)
-%     circshift(xhist,[1,0]);
-%     xhist(1)=amdemodsignal(i);
-%     filteredamdemodsignal(i)=sum(xhist.*b);
-%     if mod(i,100000) == 0%"Fortschritts"balken
-%         fprintf('%i|',i);
-%     end
-%     if mod(i,1400000) == 0
-%         fprintf('\n');
-%     end
-% end
+open b14.mat; %b.mat=400 Punkte, b200.mat=200, b14.mat=14
+b=ans.h';
+xhist=zeros(length(b),1);
+for i=1:length(amdemodsignal)
+    circshift(xhist,[1,0]);
+    xhist(1)=amdemodsignal(i);
+    filteredamdemodsignal(i)=sum(xhist.*b);
+    if mod(i,100000) == 0%"Fortschritts"balken
+        fprintf('%i|',i);
+    end
+    if mod(i,1400000) == 0
+        fprintf('\n');
+    end
+end
 %Bis hier um den Filter auszukommentieren
 clear amdemosignal b xhist
 
 %Dezimation naiv reicht, Parameter Nth muss genau austariert werden
-Nth=16;%nur jedes Nte bit nehmen
+Nth=26;%nur jedes Nte bit nehmen
 decisignal=[1:floor(size(filteredamdemodsignal)/Nth)]';
 for i=1:floor(size(filteredamdemodsignal)/Nth)
     decisignal(i)=filteredamdemodsignal(i*Nth);
@@ -48,27 +48,28 @@ clear decisignal
 
 %Carrier-Frequency-Error ausgleichen! (Highpass 15-20Hz, IIR?)
 filteredfmdemod=fmdemod;
-%FILTER FEHLT
+load('IIRCoeff.mat');
+filteredfmdemod = filter(IIRCoeff,fmdemod);
 clear fmdemod
 
 %Für das Radio kopieren und das Signal filtern (Lowpass zwischen 15-19kHz)
 tonsignal=filteredfmdemod;
 filteredtonsignal=tonsignal;
 %Ab hier den Filter auskommentieren
-% open radiotonfilter200.mat;
-% b=ans.h';
-% xhist=zeros(length(b),1);
-% for i=1:length(tonsignal)
-%     circshift(xhist,[1,0]);
-%     xhist(1)=tonsignal(i);
-%     filteredtonsignal(i)=sum(xhist.*b);
-%     if mod(i,100000) == 0%"Fortschritts"balken
-%         fprintf('%i|',i);
-%     end
-%     if mod(i,1400000) == 0
-%         fprintf('\n');
-%     end
-% end
+open radiotonfilter200.mat;
+b=ans.h';
+xhist=zeros(length(b),1);
+for i=1:length(tonsignal)
+    circshift(xhist,[1,0]);
+    xhist(1)=tonsignal(i);
+    filteredtonsignal(i)=sum(xhist.*b);
+    if mod(i,100000) == 0%"Fortschritts"balken
+        fprintf('%i|',i);
+    end
+    if mod(i,1400000) == 0
+        fprintf('\n');
+    end
+end
 %Bis hier um den Filter auszukommentieren
 clear tonsignal
 
