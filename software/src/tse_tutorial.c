cb8d160a20bb6bd3dfeee58be46b940b2bf5c7af
Alexander REZNICEK <h1125076@ti6.tilab.tuwien.ac.at>
2016-01-05 19:53:18 +0100

    src modded für HWSW-Radioaudio-Ausgabe Rev0

--- software/src/tse_tutorial.c
+++ software/src/tse_tutorial.c
@@ -12,12 +12,11 @@
 #include "system.h"
 #include "display.h"
                  
-#define SONG_LEN (32000*50) // song length (samples)
 #define BUF_SIZE 64
 
 // Allocate descriptors in the descriptor_memory (onchip memory) and rx frames (main memory)
 alt_sgdma_descriptor rx_descriptor[3]  __attribute__ (( section ( ".descriptor_memory" )));
-unsigned char rx_frame[2][BUF_SIZE]    __attribute__ (( section ( ".main_memory" )));
+unsigned char rx_audio[2][BUF_SIZE]    __attribute__ (( section ( ".main_memory" )));
 
 int main(void)
 {	
@@ -25,11 +24,9 @@ int main(void)
 	alt_sgdma_dev * sgdma_rx_dev;
 
 	unsigned char act_frame = 0;
-	//unsigned int* sample_ptr = NULL;
 
 	short* song = NULL;
 	unsigned char* song_ptr_b = NULL;
-	volatile unsigned int song_cnt = 0;
 	int sample = 0;
 	
 	unsigned int avail = 0;	
@@ -100,7 +97,7 @@ int main(void)
 			 );
 
 	// Allocate memory for the song (SDRAM)
-	song = (short*)malloc(SONG_LEN*2);
+	song = (short*)malloc(BUF_SIZE);
 	if (song == NULL)
 	{
 		display_print("Could not allocate memory for audio file!\n");
@@ -109,10 +106,9 @@ int main(void)
 	
 	// Set a pointer (byte-access) to the song
 	song_ptr_b = (unsigned char*)song;
-	song_cnt = 0;	
 	
 	// Create sgdma receive descriptor
-	alt_avalon_sgdma_construct_stream_to_mem_desc( &rx_descriptor[0], &rx_descriptor[1], (alt_u32 *)rx_frame[0], 0, 0 );
+	alt_avalon_sgdma_construct_stream_to_mem_desc( &rx_descriptor[0], &rx_descriptor[1], (alt_u32 *)rx_audio[0], BUF_SIZE, 0 );
 	
 	display_print ("Ready to receive data!\n");
 	
@@ -124,26 +120,32 @@ int main(void)
 	while(1) 
 	{
 		// Create sgdma receive descriptor
-		alt_avalon_sgdma_construct_stream_to_mem_desc( &rx_descriptor[1-act_frame], &rx_descriptor[2-act_frame], (alt_u32 *)rx_frame[1-act_frame], 0, 0 );
+		alt_avalon_sgdma_construct_stream_to_mem_desc( &rx_descriptor[1-act_frame], &rx_descriptor[2-act_frame], (alt_u32 *)rx_audio[1-act_frame], BUF_SIZE, 0 );
 		
 		// Set up non-blocking transfer of sgdma receive descriptor
 		alt_avalon_sgdma_do_async_transfer( sgdma_rx_dev, &rx_descriptor[1-act_frame] );
 
-		// Check if the frame has the required type
-		if ((rx_frame[act_frame][14] == 0x88)&&(rx_frame[act_frame][15] == 0xb5))
+		// Copy received frame to song 
+		for (i = 0; i < BUF_SIZE; i ++)
 		{
-			// Copy received frame to song 
-			for (i = 16; i < BUF_SIZE; i ++)
-			{
-				song_ptr_b[song_cnt] = rx_frame[act_frame][i];
-				song_cnt ++;
-			}
+			song_ptr_b[i] = rx_audio[act_frame][i];
+		}
 
-			if (song_cnt >= 4*SONG_LEN)
-			{
-				break;
-			}
+		
+		for (i = 0; i < BUF_SIZE/4; i ++)
+		{
+			//Play the received frame
+			avail = (unsigned int)((IORD_32DIRECT(AUDIO_BASE,4)&0xFF000000)>>24);
+			while(avail <= 0)avail = (unsigned int)((IORD_32DIRECT(AUDIO_BASE,4)&0xFF000000)>>24);
+			// Read sample from SDRAM
+			sample = (int)song[i];
+			// and write it to the FIFO for left channel
+			IOWR_32DIRECT(AUDIO_BASE,8,sample);
 
+			// Read sample from SDRAM
+			//sample = (int)song[i+1];//!MONOTON!
+			// and write it to the FIFO for left channel
+			IOWR_32DIRECT(AUDIO_BASE,12,sample);
 		}
 		
 		act_frame = 1-act_frame;
@@ -156,34 +158,5 @@ int main(void)
 			//display_print(outputBuffer);
 		}
 	}
-	
-	while(1)
-	{
-		song_cnt = 0;
-		while(1)
-		{
-			// If space for samples is available in FIFOs
-			avail = (unsigned int)((IORD_32DIRECT(AUDIO_BASE,4)&0xFF000000)>>24);
-			if (avail > 0)
-			{
-				// Read sample from SDRAM
-				sample = (int)song[song_cnt++];
-				// and write it to the FIFO for left channel
-				IOWR_32DIRECT(AUDIO_BASE,8,sample);
-
-				// Read sample from SDRAM
-				sample = (int)song[song_cnt++];
-				// and write it to the FIFO for left channel
-				IOWR_32DIRECT(AUDIO_BASE,12,sample);
-				
-				// If song length is reached
-				if (song_cnt >= 2*SONG_LEN)
-				{
-					break;
-				}
-			}
-		}	
-	}
-	
 	return 0;
 }
