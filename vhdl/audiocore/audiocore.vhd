b67bc167cd675c525dacc36999459c73f553985e
MartinMosbeck <martin.mosbeck@gmx.at>
2016-01-05 09:53:51 +0100

    Added barebone for audiocore as vhdl
    Signed-off-by: MartinMosbeck <martin.mosbeck@gmx.at>

---
+++ vhdl/audiocore/audiocore.vhd
@@ -0,0 +1,32 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity audiocore is
+	port (
+		clk   : in std_logic;
+		res_n : in std_logic;
+		
+		-- stream input
+		asin_data : in std_logic_vector(31 downto 0);
+		asin_startofpacket : in std_logic;
+		asin_endofpacket : in std_logic;
+		asin_valid : in std_logic;
+		asin_ready : out std_logic;
+		
+		-- stream output
+		asout_data : out std_logic_vector(31 downto 0);
+		asout_startofpacket : out std_logic;
+		asout_endofpacket : out std_logic;
+		asout_valid : out std_logic;
+		asout_ready : in std_logic
+	);
+end entity;
+
+architecture rtl of stream_processor is
+begin
+
+end architecture;
+
