7edbf3c30ad83706deae8a4c246bdaf84ff0d1f6
Alexander REZNICEK <h1125076@ti7.tilab.tuwien.ac.at>
2016-02-20 17:23:28 +0100

    Demodulator Reset auskommentiert; Zeile 63/64)

--- vhdl/audiocore/demodulator.vhdl
+++ vhdl/audiocore/demodulator.vhdl
@@ -60,8 +60,8 @@ begin
 			data_out_cur <= (others =>'0');
 			validout_cur <= '0';
 
-			--data_con_I <= (others => '0');
-			--data_con_Q <= (others => '0');
+			data_con_I <= (others => '0');
+			data_con_Q <= (others => '0');
 		elsif rising_edge(clk) then
 			--internals
 				data_out_cur <= data_out_next;
