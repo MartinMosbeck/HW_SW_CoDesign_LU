c9500fa205171480efc8a77d2e3fe740a5132a5e
Alexander Reznicek <e1125076@student.tuwien.ac.at>
2016-02-22 23:25:32 +0100

    Radiodecoder.c: nicht mehr benötigten/alten Code entfernt

--- tools/HWinC/Radiodecoder.c
+++ tools/HWinC/Radiodecoder.c
@@ -3,61 +3,6 @@
 #include <stdlib.h>
 #include <math.h>//Für pow()
 
-int lookup_sin[25]={
-	0b00000000000000000000000000000000,
-	0b00000000111111110111111010101101,
-	0b00000000001000000001010111010110,
-	0b11111111000001001000100011010011,
-	0b11111111110000000101010111011101,
-	0b00000000111100110111100001110000,
-	0b00000000010111100011110101101001,
-	0b11111111000110000101110101000010,
-	0b11111111100001001010101111001011,
-	0b00000000110110000010010111011111,
-	0b00000000100101100111100100011000,
-	0b11111111001110101011111110100101,
-	0b11111111010100001100000110000110,
-	0b00000000101011110011111001111010,
-	0b00000000110001010100000001011011,
-	0b11111111011010011000011011101000,
-	0b11111111001001111101101000100001,
-	0b00000000011110110101010000110101,
-	0b00000000111001111010001010111110,
-	0b11111111101000011100001010010111,
-	0b11111111000011001000011110010000,
-	0b00000000001111111010101000100011,
-	0b00000000111110110111011100101101,
-	0b11111111110111111110101000101010,
-	0b11111111000000001000000101010011
-};
-int lookup_cos[25]={
-	0b00000001000000000000000000000000,
-	0b00000000000100000001001100001010,
-	0b11111111000000100000010011000110,
-	0b11111111110100000000011111001000,
-	0b00000000111101111111010100010000,
-	0b00000000010011110001101110111100,
-	0b11111111000100011111101000101100,
-	0b11111111100100110000000000100001,
-	0b00000000111000000101010110100010,
-	0b00000000100010010010101111110001,
-	0b11111111001100001110010001000100,
-	0b11111111010111001101000111001001,
-	0b00000000101110101001110110110000,
-	0b00000000101110101001110110110000,
-	0b11111111010111001101000111001001,
-	0b11111111001100001110010001000100,
-	0b00000000100010010010101111110001,
-	0b00000000111000000101010110100010,
-	0b11111111100100110000000000100001,
-	0b11111111000100011111101000101100,
-	0b00000000010011110001101110111100,
-	0b00000000111101111111010100010000,
-	0b11111111110100000000011111001000,
-	0b11111111000000100000010011000110,
-	0b00000000000100000001001100001010
-};
-
 int lookup_sin16[25]={
         0b00000000000000000000000000000000,
         0b00000000000000001111111101111110,
@@ -115,34 +60,25 @@ int lookup_cos16[25]={
 };
 //dafür ist -lm beim compilieren nötig
 //==gcc Radiodecoder.c -lm -g -o Radiodecoder
+#define ANZ_NACHKOMMA 16//Gesamt 32 bit,d.h. der Rest ist Vorkomma
 float zeigeFixpoint(uint32_t x){
 	float zahl=0;
 	int i;
 	if(x&0x80000000){//Negative Zahl
-		zahl=-32768;//-128;
+		zahl-=pow(2,32-ANZ_NACHKOMMA);
 	}
 	for(i=0; i<31; i++){
 		if(1<<i & x){
-			zahl+=pow(2,i-16);//24);
+			zahl+=pow(2,i-ANZ_NACHKOMMA);
 		}
 	}
 	//printf("%3.12f\n",zahl);
 	return zahl;
 }
-//Mathematisch korrekt?
-/*uint32_t fixpoint_mult(uint32_t a, uint32_t b){
-	uint64_t ergebnis=0, i;
-	for(i=0; i<32; i++){
-		if(1<<i & b){
-			ergebnis+=((uint64_t)a<<i);
-		}
-	}
-	return (ergebnis&0x00FFFFFFFF000000)>>24;
-}*/
 //Mit signconvert, was unsere FPGA-HW auch macht
 int fixpoint_mult(int a, int b){
 	long ergebnis=(long)a*(long)b;
-	return ergebnis>>16;//>>24;
+	return ergebnis>>16;
 }
 
 int main(int argc, char *argv[]){
@@ -153,7 +89,7 @@ int main(int argc, char *argv[]){
 	if(NULL==eingabeDatei)printf("Datei kann nicht geöffnet werden!\n");
 	fseek(eingabeDatei,0L,SEEK_END);
 	size_t anzBytes=ftell(eingabeDatei)/2;
-	fseek(eingabeDatei,0L,SEEK_SET);//rewind(eingabeDatei);
+	fseek(eingabeDatei,0L,SEEK_SET);
 	uint8_t c[2]="";
 	uint8_t * I=malloc(anzBytes/2);
 	uint8_t * Q=malloc(anzBytes/2);
@@ -184,8 +120,8 @@ int main(int argc, char *argv[]){
 	for(i=0; i<anzBytes/2; ++i){
 		//printf("I[%i]=%i, Q[%i]=%i\n",i,I[i],i,Q[i]);
 		//Mixer
-		Itemp=(I[i]-127)<<16;//<<24;
-		Qtemp=(Q[i]-127)<<16;//<<24;
+		Itemp=(I[i]-127)<<16;
+		Qtemp=(Q[i]-127)<<16;
 		//printf("I[%i]-127=%i, Q[%i]-127=%i\n",i,I[i]-127,i,Q[i]-127);
 		//printf("Itemp = %f, Qtemp = %f\n", zeigeFixpoint(Itemp), zeigeFixpoint(Qtemp));
 		
@@ -211,12 +147,9 @@ int main(int argc, char *argv[]){
 		
 		//Demodulator - ab hier nur wenns decimiertes ist weiterverarbeiten
 		if(valid){
-			//HW-Original (es wird damit der Realteil=I genommen)
-			//demodulated=fixpoint_mult(Iout,I_con) - fixpoint_mult(Qout,Q_con);
-			//Eigentlich soll aber der Imaginärteil(=Q) des Programmes genommen werden
 			demodulated=fixpoint_mult(Iout,Q_con) + fixpoint_mult(Qout,I_con);
 			I_con=Iout;
-			Q_con=fixpoint_mult(0xFFFF0000,Qout);//0xFF000000,Qout);
+			Q_con=fixpoint_mult(0xFFFF0000,Qout);
 			//printf("demodulated = %f, I_con = %f, Q_con = %f\n",zeigeFixpoint(demodulated),zeigeFixpoint(I_con),zeigeFixpoint(Q_con));
 		}
 		
@@ -225,10 +158,9 @@ int main(int argc, char *argv[]){
 			validvalid=1-validvalid;//jedes zweite nehmen (=Decimator)
 			if(validvalid){
 				//printf("Outputlogic sagt ja!\n");
-				//data_fixp=fixpoint_mult(demodulated,0x001E0000);//0x1E000000);
 				data_fixp=fixpoint_mult(demodulated,0x00000300);
 				//printf("data_fixp = %f\n",zeigeFixpoint(data_fixp));
-				outputvector[outputpos++]=(data_fixp + 0b00000000011111110000000000000000)>>16;//0b01111111000000000000000000000000)>>24;
+				outputvector[outputpos++]=(data_fixp + 0b00000000011111110000000000000000)>>16;
 				//printf("\n");
 			}
 		}
