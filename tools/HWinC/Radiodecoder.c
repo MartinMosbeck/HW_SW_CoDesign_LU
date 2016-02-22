91805795fa41457a93a93a46f5bfab034ce5e38d
Alexander Reznicek <e1125076@student.tuwien.ac.at>
2016-02-22 19:00:36 +0100

    Radiodecoder.c debuggt und funktionsfÃhig (ohne Filter mit extrem starkem Rauschen). Ton in Matlab aber anhÃ¶rbar, auf demselben Stand wie die FPGA-HW aus dem vorherigen Commit (die aber deutlich schlechteren Ton = fast unhÃrbar) erzeugt.

--- tools/HWinC/Radiodecoder.c
+++ tools/HWinC/Radiodecoder.c
@@ -57,17 +57,73 @@ int lookup_cos[25]={
 	0b11111111000000100000010011000110,
 	0b00000000000100000001001100001010
 };
+
+int lookup_sin16[25]={
+        0b00000000000000000000000000000000,
+        0b00000000000000001111111101111110,
+        0b00000000000000000010000000010101,
+        0b11111111111111110000010010001001,
+        0b11111111111111111100000001010110,
+        0b00000000000000001111001101111000,
+        0b00000000000000000101111000111101,
+        0b11111111111111110001100001011110,
+        0b11111111111111111000010010101100,
+        0b00000000000000001101100000100101,
+        0b00000000000000001001011001111001,
+        0b11111111111111110011101011000000,
+        0b11111111111111110101000011000010,
+        0b00000000000000001010111100111110,
+        0b00000000000000001100010101000000,
+        0b11111111111111110110100110000111,
+        0b11111111111111110010011111011011,
+        0b00000000000000000111101101010100,
+        0b00000000000000001110011110100010,
+        0b11111111111111111010000111000011,
+        0b11111111111111110000110010001000,
+        0b00000000000000000011111110101010,
+        0b00000000000000001111101101110111,
+        0b11111111111111111101111111101011,
+        0b11111111111111110000000010000010
+
+};
+int lookup_cos16[25]={
+        0b00000000000000010000000000000000,
+        0b00000000000000000001000000010011,
+        0b11111111111111110000001000000101,
+        0b11111111111111111101000000001000,
+        0b00000000000000001111011111110101,
+        0b00000000000000000100111100011011,
+        0b11111111111111110001000111111011,
+        0b11111111111111111001001100000001,
+        0b00000000000000001110000001010101,
+        0b00000000000000001000100100101011,
+        0b11111111111111110011000011100101,
+        0b11111111111111110101110011010010,
+        0b00000000000000001011101010011101,
+        0b00000000000000001011101010011101,
+        0b11111111111111110101110011010010,
+        0b11111111111111110011000011100101,
+        0b00000000000000001000100100101011,
+        0b00000000000000001110000001010101,
+        0b11111111111111111001001100000001,
+        0b11111111111111110001000111111011,
+        0b00000000000000000100111100011011,
+        0b00000000000000001111011111110101,
+        0b11111111111111111101000000001000,
+        0b11111111111111110000001000000101,
+        0b00000000000000000001000000010011
+};
 //dafür ist -lm beim compilieren nötig
 //==gcc Radiodecoder.c -lm -g -o Radiodecoder
 float zeigeFixpoint(uint32_t x){
 	float zahl=0;
 	int i;
 	if(x&0x80000000){//Negative Zahl
-		zahl=-128;
+		zahl=-32768;//-128;
 	}
 	for(i=0; i<31; i++){
 		if(1<<i & x){
-			zahl+=pow(2,i-24);
+			zahl+=pow(2,i-16);//24);
 		}
 	}
 	//printf("%3.12f\n",zahl);
@@ -86,7 +142,7 @@ float zeigeFixpoint(uint32_t x){
 //Mit signconvert, was unsere FPGA-HW auch macht
 int fixpoint_mult(int a, int b){
 	long ergebnis=(long)a*(long)b;
-	return ergebnis>>24;
+	return ergebnis>>16;//>>24;
 }
 
 int main(int argc, char *argv[]){
@@ -126,20 +182,28 @@ int main(int argc, char *argv[]){
 	uint8_t * outputvector=malloc(anzBytes/2/20/2);
 	int outputpos=0;
 	for(i=0; i<anzBytes/2; ++i){
+		//printf("I[%i]=%i, Q[%i]=%i\n",i,I[i],i,Q[i]);
 		//Mixer
-		Itemp=(I[i]-127)<<24;
-		Qtemp=(Q[i]-127)<<24;
+		Itemp=(I[i]-127)<<16;//<<24;
+		Qtemp=(Q[i]-127)<<16;//<<24;
+		//printf("I[%i]-127=%i, Q[%i]-127=%i\n",i,I[i]-127,i,Q[i]-127);
+		//printf("Itemp = %f, Qtemp = %f\n", zeigeFixpoint(Itemp), zeigeFixpoint(Qtemp));
 		
-		Iout=fixpoint_mult(Itemp,lookup_cos[t_cur]) - fixpoint_mult(Qtemp,lookup_sin[t_cur]);
-		Qout=fixpoint_mult(Itemp,lookup_sin[t_cur]) + fixpoint_mult(Qtemp,lookup_cos[t_cur]);
+		Iout=fixpoint_mult(Itemp,lookup_cos16[t_cur]) - fixpoint_mult(Qtemp,lookup_sin16[t_cur]);
+		Qout=fixpoint_mult(Itemp,lookup_sin16[t_cur]) + fixpoint_mult(Qtemp,lookup_cos16[t_cur]);
+		//printf("Iout = %f, Qout = %f\n", zeigeFixpoint(Iout), zeigeFixpoint(Qout));
 		
 		if (24==t_cur) t_cur=0;
 		else t_cur++;
 	
 		//Decimator
 		if(20-1==deci_cnt){
+			//printf("#%i\n",i+1);
+			//printf("Iout = %f, Qout = %f\n", zeigeFixpoint(Iout), zeigeFixpoint(Qout));
+			//printf("I_con = %f, Q_con = %f\n", zeigeFixpoint(I_con), zeigeFixpoint(Q_con));
 			valid=1;
 			deci_cnt=0;
+			//printf("Decimator sagt ja!\n");
 		}else{
 			valid=0;
 			deci_cnt++;
@@ -148,19 +212,24 @@ int main(int argc, char *argv[]){
 		//Demodulator - ab hier nur wenns decimiertes ist weiterverarbeiten
 		if(valid){
 			//HW-Original (es wird damit der Realteil=I genommen)
-			demodulated=fixpoint_mult(Iout,I_con) - fixpoint_mult(Qout,Q_con);
+			//demodulated=fixpoint_mult(Iout,I_con) - fixpoint_mult(Qout,Q_con);
 			//Eigentlich soll aber der Imaginärteil(=Q) des Programmes genommen werden
-			//demodulated=fixpoint_mult(Iout,Q_con) + fixpoint_mult(Qout,I_con);
+			demodulated=fixpoint_mult(Iout,Q_con) + fixpoint_mult(Qout,I_con);
 			I_con=Iout;
-			Q_con=fixpoint_mult(0xFF000000,Qout);
+			Q_con=fixpoint_mult(0xFFFF0000,Qout);//0xFF000000,Qout);
+			//printf("demodulated = %f, I_con = %f, Q_con = %f\n",zeigeFixpoint(demodulated),zeigeFixpoint(I_con),zeigeFixpoint(Q_con));
 		}
 		
 		//Outlogic
 		if(valid){
 			validvalid=1-validvalid;//jedes zweite nehmen (=Decimator)
 			if(validvalid){
-				data_fixp=fixpoint_mult(demodulated,0x1E000000);
-				outputvector[outputpos++]=(data_fixp + 0b01111111000000000000000000000000)>>24;
+				//printf("Outputlogic sagt ja!\n");
+				//data_fixp=fixpoint_mult(demodulated,0x001E0000);//0x1E000000);
+				data_fixp=fixpoint_mult(demodulated,0x00000300);
+				//printf("data_fixp = %f\n",zeigeFixpoint(data_fixp));
+				outputvector[outputpos++]=(data_fixp + 0b01111111000000000000000000000000)>>24;//>>24;
+				//printf("\n");
 			}
 		}
 	}
