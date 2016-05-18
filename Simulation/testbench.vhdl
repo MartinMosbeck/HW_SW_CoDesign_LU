0e883bbadb89f440611de66faa0cf8c39ea65e81
Alexander Reznicek <1125076@github.com>
2016-05-18 20:24:37 +0200

    IIR-Filter staged hinzugefügt - Simulation OK, Code faktorisieren, Synthetisieren noch nötig
    Signed-off-by: Alexander Reznicek <1125076@github.com>

--- Simulation/testbench.vhdl
+++ Simulation/testbench.vhdl
@@ -11,183 +11,159 @@ entity audiocore_Simulation is
 end audiocore_Simulation;
 
 architecture testbench of audiocore_Simulation is
+
+  -- Set up the signals on the 3bit_counter
+  signal button1 : std_logic;
+  signal button4 : std_logic;
+  signal led1    : std_logic;
+  signal led2    : std_logic;
+  signal led3    : std_logic;
+
+  -- Set up the vcc signal as 1
+  signal vcc  : std_logic := '1';
   
-  signal clk: std_logic:='0';
+  signal clk: std_logic;
   signal res: std_logic;
   
-  signal Istart, Iend, Ivalid, Iready, Ostart, Oend, Ovalid, Oready: std_logic;
-  signal Idata, Odata: std_logic_vector(31 downto 0);
+  signal Iin: fixpoint;
+  signal validin: std_logic;
+  
+  signal Iout: fixpoint;
+  signal validout: std_logic;
   
   signal counter: integer:=30000;
   
   begin
-    dut : entity work.audiocore
+    dut : entity work.IIRFilter
 		port map
 		(
-		clk   => clk,
+		clk => clk,
 		res_n => res,
-		
-		-- stream input
-		asin_data => Idata,
-		asin_startofpacket => Istart,
-		asin_endofpacket => Iend,
-		asin_valid => Ivalid,
-		asin_ready => Iready,
 
-		-- stream output
-		asout_data => Odata,
-		asout_startofpacket => Ostart,
-		asout_endofpacket => Oend,
-		asout_valid => Ovalid,
-		asout_ready => Oready
-	);
+		data_in => Iin,
+		validin => validin,
+
+		data_out => Iout,
+		validout => validout
+		);
     
     stimulus : process is
       begin
 		res <= '0'; wait for 20 ns;
 		res <= '1'; wait for 20 ns;
-		Ivalid <= '1';
-		Oready <= '1';
+		validin <= '1';
 
-		Istart <= '1';
-		Iend <= '0';
-		
-		Idata <= x"00000000";
+		Iin <= x"ffe20000";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Istart <= '0';
-		Idata <= x"00000000";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"00000000";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"000088b5";
+		Iin <= x"ffde4845";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
 		
-		Idata <= x"61bdc2a5";
+		
+		validin <= '0';
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"a1423a44";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"57afd3c3";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"bc544d37";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"3b9baad3";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"d476651e";
+		validin <= '1';
+		
+		Iin <= x"ffe5e9ab";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"3b828ae6";
+		Iin <= x"ffd2f96b";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"d7969118";
+		Iin <= x"ffe53198";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffd948fc";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffd71d0e";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffd4243e";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
 		
-		Idata <= x"2c4f6ad7";
+		validin <= '0';
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"c0afae37";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"4d3149ae";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"b1c4a952";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"583f57ad";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"a6bdbb57";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"5434529c";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"babfbc68";
-		Iend <= '1';
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
+		validin <= '1';
 		
-		Istart <= '1';
-		Iend <= '0';
+		Iin <= x"ffd1e69c";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffd01e15";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffc0864a";
+		counter <= counter + 10000;
+		clk <= '1'; wait for 5 ns;
+		clk <= '0'; wait for 5 ns;
+		Iin <= x"ffc5d56c";
 		
-		Idata <= x"00000000";
+		validin <= '0';
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Istart <= '0';
-		Idata <= x"00000000";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"00000000";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"000088b5";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
+		validin <= '1';
 		
-		Idata <= x"59322f89";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"a9ccd175";
+		Iin <= x"ffd07c06";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"6d25396e";
+		Iin <= x"ffc182cd";
+		counter <= counter + 10000;
 		clk <= '1'; wait for 5 ns;
 		clk <= '0'; wait for 5 ns;
-		Idata <= x"80d3e1a2";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"8e17264f";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"6cbfb7a7";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"a5363b3e";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"51beb6b2";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		
-		Idata <= x"a44a5b44";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"4ea7b8aa";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"ac472d53";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"60b3c2a5";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"b2513336";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"37acc3cd";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"bf5d472b";
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Idata <= x"3d96acd4";
-		Iend <= '1';
-		clk <= '1'; wait for 5 ns;
-		clk <= '0'; wait for 5 ns;
-		Iend <= '0';
 		  
-		Ivalid <= '0';
+		validin <= '0';
 		
+		report integer'image(to_integer(unsigned(Iout)));
+		--report integer'image(to_integer(unsigned(Qout)));
+		--endloop
 		loop
 		  clk <= '1'; wait for 5 ns;
 		  clk <= '0'; wait for 5 ns;
--- vhdl/audiocore/IIRFilter.vhdl
+++ vhdl/audiocore/IIRFilter.vhdl
@@ -29,7 +29,7 @@ architecture behavior of IIRFIlter is
 		return result_full(47 downto 16);
 	end function;
 	
-	constant order: natural := 4;
+	constant order: natural := 4;--ist eigentlich 5 (order-1)
 	function a(index:index) 
 		return fixpoint is
 	begin 
@@ -67,53 +67,102 @@ architecture behavior of IIRFIlter is
 	signal xhist_cur,xhist_next : fixpoint_array (order downto 0) := (others =>  (others => '0'));
 	signal yhist_cur,yhist_next : fixpoint_array (order-1 downto 0) := (others => (others => '0'));
 
+	signal valid_array_cur, valid_array_next: std_logic_vector(2*order-1 downto 0) := (others => '0');
+	
+	signal data_out_array_cur, data_out_array_next: fixpoint_array(2*order-1 downto 0);
+
 	signal data_out_cur, data_out_next : fixpoint;
 	
 	signal validout_cur, validout_next: std_logic;
+	
+	type datashift_array is array(natural range <>) of integer;
+	signal shift_array_x_cur, shift_array_x_next: datashift_array(order-1 downto 0) := (others => order-1);
+	signal shift_array_y_cur, shift_array_y_next: datashift_array(order downto 0) := (others => 0);
+	
+	signal start_flag, start_flag_next, startout_flag, startout_flag_next: std_logic:='0';
 
 begin
-	compute: process (validin,data_in, validout_cur, xhist_cur, yhist_cur, data_out_cur)
+	compute: process (validin,data_in, validout_cur, xhist_cur, yhist_cur, data_out_cur,valid_array_cur, shift_array_x_cur, shift_array_y_cur,data_out_array_cur,startout_flag,start_flag)
 		variable xhist_temp : fixpoint_array(order downto 0);
 		variable data_out_temp : fixpoint;
 	begin
-		data_out_next <= data_out_cur;
-		validout_next <= validout_cur;
 		xhist_next <= xhist_cur;
 		yhist_next <= yhist_cur;
+		
+		start_flag_next <= start_flag;
+		startout_flag_next <= startout_flag;
+		
+		validout_next <= valid_array_cur(2*order-1);
+		data_out_next <= data_out_cur;
+		valid_array_next(2*order-1 downto 1) <= valid_array_cur(2*order-2 downto 0);
+		valid_array_next(0) <= validin;
+		data_out_array_next(0) <= (others => '0');
+		
+		for i in 1 to order-1 loop
+			if(validin = '1') then
+				shift_array_x_next(i) <= shift_array_x_cur(i-1);
+			else
+				shift_array_x_next(i) <= shift_array_x_cur(i-1)-1;
+			end if;
+		end loop;
+		      
+		if(valid_array_cur(order-1) = '1') then
+		    start_flag_next <= '1';
+		end if;
+		
+		if(valid_array_cur(2*order-1) = '1')then
+		  startout_flag_next <= '1';
+		end if;
+		
+		if(start_flag = '1' and startout_flag = '1' and valid_array_cur(order-1) = '0'  and valid_array_cur(2*order-1) = '0')then
+				shift_array_y_next(0) <= shift_array_y_cur(0);
+		elsif(start_flag = '1' and valid_array_cur(order-1) = '0'  and shift_array_y_cur(0) < order)then
+				shift_array_y_next(0) <= shift_array_y_cur(0)+1;
+                elsif(startout_flag = '1' and valid_array_cur(2*order-1) = '0' and shift_array_y_cur(0) >0) then
+				shift_array_y_next(0) <= shift_array_y_cur(0)-1;
+                end if;
+		for i in 1 to order loop
+                                if(valid_array_cur(2*order-1) = '0' and shift_array_y_cur(i-1) >0) then
+                                shift_array_y_next(i) <= shift_array_y_cur(i-1)-1;
+                                else
+                                shift_array_y_next(i) <= shift_array_y_cur(i-1);
+                                end if;
+		end loop;
 
+		--Vorbereitung
 		if(validin = '1') then
-			data_out_temp := (others => '0');
-
 			--shift xhist
 			for i in 1 to order loop
-				xhist_temp(i) := xhist_cur(i-1);
+				xhist_next(i) <= xhist_cur(i-1);
 			end loop;
 
-			xhist_temp(0) := data_in;
+			xhist_next(0) <= data_in;
 			
-			--add up
-
-			for i in 0 to order loop
-				data_out_temp := data_out_temp + fixpoint_mult(xhist_temp(i),b(i));
-			end loop;
-
-			for i in 0 to order-1 loop
-				data_out_temp := data_out_temp - fixpoint_mult(yhist_cur(i),a(i));
-			end loop;
+			data_out_array_next(0)<=fixpoint_mult(xhist_cur(order-1),b(order));
+		end if;
+		
+		--add up
+		--Invarianten
+		for i in 1 to order loop
+			data_out_array_next(i)<=data_out_array_cur(i-1)+fixpoint_mult(xhist_cur(shift_array_x_cur(i-1)),b(order-i));
+		end loop;
+		for i in 1 to order-1 loop
+			if(shift_array_y_cur(i-1) < order) then
+			  data_out_array_next(i+order)<=data_out_array_cur(i+order-1) - fixpoint_mult(yhist_cur(shift_array_y_cur(i-1)),a(order-i));
+			end if;
+		end loop;
+		
+		--Nachbereitung
+		if(valid_array_cur(2*order-1) = '1') then
+			data_out_temp := data_out_array_cur(2*order-1) - fixpoint_mult(yhist_cur(shift_array_y_cur(order)),a(0));
 
 			--shift yhist
 			for i in 1 to order-1 loop
 				yhist_next(i) <= yhist_cur(i-1);
 			end loop; 
-		
-			yhist_next(0) <= data_out_temp;
-
-			data_out_next <= data_out_temp;
-			validout_next <= '1';
 			
-			xhist_next <= xhist_temp;
-		else
-			validout_next <= '0';
+			data_out_next  <= data_out_temp;
+			yhist_next(0) <= data_out_temp;
 		end if;
 
 	end process compute;
@@ -130,6 +179,14 @@ begin
 			yhist_cur <= yhist_next;
 			data_out_cur <= data_out_next;
 			validout_cur <= validout_next;
+			
+			valid_array_cur<= valid_array_next;
+			data_out_array_cur<=data_out_array_next;
+			shift_array_x_cur<=shift_array_x_next;
+			shift_array_y_cur<=shift_array_y_next;
+			
+			start_flag <= start_flag_next;
+			startout_flag <= startout_flag_next;
 			
 			data_out <= data_out_next;
 			validout <= validout_next;
